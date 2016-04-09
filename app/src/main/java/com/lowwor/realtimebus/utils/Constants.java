34861c3bf66f5b737c91bd6e77f0d733dd2edf52
lowworker <lowworker@163.com>
2016-04-09 17:04:42 +0800

    refactor: remove useless class

--- app/src/main/java/com/lowwor/realtimebus/utils/Constants.java
+++
@@ -1,8 +0,0 @@
-package com.lowwor.realtimebus.utils;
-
-/**
- * Created by lowworker on 2015/10/17.
- */
-public class Constants {
-    public static final int REFRESH_INTERVAL = 3;
-}
--- app/src/main/java/com/lowwor/realtimebus/viewmodel/NotificationView.java
+++
@@ -1,8 +0,0 @@
-package com.lowwor.realtimebus.viewmodel;
-
-/**
- * Created by lowworker on 2016/3/4 0004.
- */
-public interface NotificationView {
-    void showNotification(String busStationName);
-}
