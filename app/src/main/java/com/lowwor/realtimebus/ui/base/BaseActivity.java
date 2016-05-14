b11ca41d3d8a8ca5c56ad891b96ddb58c1b7973a
lowwor <lowwor@foxmail.com>
2016-05-14 16:52:50 +0800

    fix: remove umeng

--- app/src/main/java/com/lowwor/realtimebus/ui/base/BaseActivity.java
+++ app/src/main/java/com/lowwor/realtimebus/ui/base/BaseActivity.java
@@ -2,18 +2,14 @@ package com.lowwor.realtimebus.ui.base;
 
 import android.support.v7.app.AppCompatActivity;
 
-import com.umeng.analytics.MobclickAgent;
-
 /**
  * Created by lowworker on 2015/10/15.
  */
 public class BaseActivity extends AppCompatActivity {
     public void onResume() {
         super.onResume();
-        MobclickAgent.onResume(this);
     }
     public void onPause() {
         super.onPause();
-        MobclickAgent.onPause(this);
     }
 }
